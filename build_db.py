b7d37d8404c731dc9d6a8fcc0eedf097b1b85f1f
J. Nathan Allen <nateonmission@gmail.com>
2019-11-07 19:40:04 -0500

    more db work2

--- build_db.py
+++ build_db.py
@@ -3,7 +3,7 @@ import sqlite3
 from sqlite3 import Error
 
 
-def build_db(db_name):
+def build_db(db_name, data_list):
     def sql_connection(db_name):
         try:
             db = sqlite3.connect(db_name)
@@ -35,7 +35,14 @@ def load_data_into_db(db_name, data_list):
 
     def insert_data(db, data_list):
         cursor_obj = db.cursor()
-        cursor_obj.execute(
-            "INSERT INTO music(id integer PRIMARY KEY, url text, week_id date, chart_position intiger, song_title text, artist text, song_id text, prev_week intiger, peak_position intiger, weeks_on_chart intiger)"
-        )
-        db.commit()
+        i = 0
+        for list_item in data_list:
+            print(list_item)
+            # cursor_obj.execute(
+            #     f"INSERT INTO music VALUES({i}, {list_item[0]}, {list[1]}, {list[2]}, {list[3]}, {list[4]}, {list[5]}, {list[6]}, {list[7]}, {list[8]}, {list[9]} )"
+            # )
+            # i += 1
+            # db.commit()
+
+    db = sql_connection(db_name)
+    insert_data(db, data_list)
