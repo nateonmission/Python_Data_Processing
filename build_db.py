da7a25370d34d17de99bfe55752e95e167cd6fc6
J. Nathan Allen <nateonmission@gmail.com>
2019-11-12 07:30:23 -0500

    data loads into DB

--- build_db.py
+++ build_db.py
@@ -1,5 +1,6 @@
 # TASK 2 - LOAD DATA INTO A SQL DATABASE
 import sqlite3
+from datetime import datetime
 from sqlite3 import Error
 
 
@@ -15,7 +16,7 @@ def build_db(db_name, data_list):
     def create_sql_table(db):
         cursor_obj = db.cursor()
         cursor_obj.execute(
-            "CREATE TABLE music(id integer PRIMARY KEY, url text, week_id date, chart_position intiger, song_title text, artist text, song_id text, prev_week intiger, peak_position intiger, weeks_on_chart intiger)"
+            "CREATE TABLE music(id integer PRIMARY KEY, url text, week_id text, chart_position intiger, song_title text, artist text, song_id text)"
         )
         db.commit()
 
@@ -37,9 +38,32 @@ def load_data_into_db(db_name, data_list):
         cursor_obj = db.cursor()
         i = 0
         for list_item in data_list:
-            # print(list_item[3])
+            datetime_object = datetime.strptime(list_item[1], '%m/%d/%Y')
+            index = i
+            print(index)
+            print(type(index))
+            url = str(list_item[0])
+            print(url)
+            print(type(url))
+            date_ref = str(datetime_object)
+            print(date_ref)
+            print(type(date_ref))
+            chart_pos = int(list_item[2])
+            print(chart_pos)
+            print(type(chart_pos))
+            title = list_item[3]
+            print(title)
+            print(type(title))
+            artist = list_item[4]
+            print(artist)
+            print(type(artist))
+            song_id = list_item[5]
+            print(song_id)
+            print(type(song_id))
             cursor_obj.execute(
-                f"INSERT INTO music VALUES({i}, {list_item[0]}, {list_item[1]}, {list_item[2]}, {list_item[3]}, {list_item[4]}, {list_item[5]}, {list_item[6]}, {list_item[7]}, {list_item[8]} )"
+                f'INSERT INTO music VALUES(?,?,?,?,?,?,?)', (
+                    index, url, date_ref, chart_pos, title, artist, song_id
+                )
             )
             i += 1
             db.commit()
